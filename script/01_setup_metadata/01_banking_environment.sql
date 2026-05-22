4ca82c05b9243c0e1e147de4fd20ecc908636188
houda-naimi <houda1.naimi@gmail.com>
2026-05-22 10:51:36 +0000

    Add Silver layer incremental processing with watermark support

--- script/01_setup_metadata/01_banking_environment.sql
+++ script/01_setup_metadata/01_banking_environment.sql
@@ -3,7 +3,10 @@
 -- CATALOG & SCHEMA & VOLUME
 -- =====================================================
 CREATE CATALOG IF NOT EXISTS banking;
+CREATE VOLUME IF NOT EXISTS banking.source.volume;
 CREATE SCHEMA IF NOT EXISTS banking.metadata;
 CREATE SCHEMA IF NOT EXISTS banking.source;
 CREATE SCHEMA IF NOT EXISTS banking.bronze;
-CREATE VOLUME IF NOT EXISTS banking.source.volume;
+CREATE SCHEMA IF NOT EXISTS banking.silver;
+CREATE SCHEMA IF NOT EXISTS banking.gold;
+
