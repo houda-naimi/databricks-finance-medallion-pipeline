e47032fd3793ebad5e7102d123d1e524b846e891
houda-naimi <houda1.naimi@gmail.com>
2026-05-22 10:25:58 +0000

    source to bronze layer

--- script/01_setup_metadata/01_banking_environment.sql
+++ script/01_setup_metadata/01_banking_environment.sql
@@ -5,4 +5,5 @@
 CREATE CATALOG IF NOT EXISTS banking;
 CREATE SCHEMA IF NOT EXISTS banking.metadata;
 CREATE SCHEMA IF NOT EXISTS banking.source;
+CREATE SCHEMA IF NOT EXISTS banking.bronze;
 CREATE VOLUME IF NOT EXISTS banking.source.volume;
